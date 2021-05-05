0667b4f8144f483ccf36091e1b347525348ef214
gyh <uhodz@student.kit.edu>
2021-05-05 12:22:46 +0200

    add log folder command

--- cma_BallInACup.py
+++ cma_BallInACup.py
@@ -1,9 +1,10 @@
+import os
 import cma
 import gym
 import numpy as np
-
+from torch.utils.tensorboard import SummaryWriter
 from alr_envs.utils.mp_env_async_sampler import AlrMpEnvSampler
-from alr_envs.mujoco.ball_in_a_cup import ball_in_a_cup
+#from alr_envs.mujoco.ball_in_a_cup import ball_in_a_cup
 
 if __name__ == "__main__":
 
@@ -18,6 +19,23 @@ if __name__ == "__main__":
     fitness = []
     params = np.random.randn(1, dim)
     algo = cma.CMAEvolutionStrategy(x0=params, sigma0=0.1, inopts={"popsize": 14})
+
+    #write tensorboard
+    env_log_index = env_name.index(':')
+    env_log_name = env_name[env_log_index+1:]
+    path = "logs/"
+    folders = os.listdir(path)
+    print(folders)
+    if folders == []:
+        path = path + env_log_name + "_1"
+    else:
+        s = 0
+        for folder in folders:
+            if int(folder[-1]) > s:
+                s = int(folder[-1])
+        s += 1
+        path = path + env_log_name + '_' + str(s)
+    log_writer = SummaryWriter(path)
 
     t = 0
     opt = 1e10
@@ -34,8 +52,11 @@ if __name__ == "__main__":
         algo.tell(solutions, fitness)
         _, opt, __, ___ = sampler(algo.mean)
         print("opt", opt)
-        np.save("algo_mean.npy", algo.mean)
+       
+        np.save(path + "/algo_mean.npy", algo.mean)
+        log_writer.add_scalar("iteration/reward", opt, t)
+       
         fitness = []
         t += 1
 
-    np.save("algo_mean.npy", algo.mean)
+    np.save(path + "/algo_mean.npy", algo.mean)
