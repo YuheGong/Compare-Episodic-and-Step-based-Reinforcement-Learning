44451cae5c0d2ac95cc4b1d1ddcc7aa549bfd695
gyh <uhodz@student.kit.edu>
2021-05-19 17:29:51 +0200

    add tensorboard_callback

--- ppo_BIAC.py
+++ ppo_BIAC.py
@@ -12,6 +12,30 @@ import pickle
 import tensorflow as tf
 import alr_envs
 from utils.logger import logging
+import numpy as np
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+
+class TensorboardCallback(BaseCallback):
+    """
+    Custom callback for plotting additional values in tensorboard.
+    """
+
+    def __init__(self, verbose=0):
+        super(TensorboardCallback, self).__init__(verbose)
+
+    def _on_step(self) -> bool:
+        # Log scalar value (here a random variable)
+        last_dist = np.mean([self.model.env.venv.envs[i].last_dist \
+                             for i in range(len(self.model.env.venv.envs))])
+        last_dist_final = np.mean([self.model.env.venv.envs[i].last_dist_final \
+                                   for i in range(len(self.model.env.venv.envs))])
+        self.logger.record('reward/last_dist', last_dist)
+        self.logger.record('reward/last_dist_final', last_dist_final)
+        return True
+
+
 
 if __name__ == "__main__":
 
@@ -28,10 +52,10 @@ if __name__ == "__main__":
     algorithm = 'ppo'
     path = logging(env_name, algorithm)
 
-    n_cpu = 1
+    n_cpu = 8
     env = DummyVecEnv(env_fns=[make_env(env_name, path, i) for i in range(n_cpu)])
     env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
-    #env.render("human")
+    # env.render("human")
     ALGOS = {
         'a2c': A2C,
         'dqn': DQN,
@@ -46,9 +70,9 @@ if __name__ == "__main__":
     model = ALGO(MlpPolicy, env, verbose=1,
                 # policy_kwargs=policy_kwargs,
                 tensorboard_log= path,
-                learning_rate=0.00001,
+                learning_rate=0.0001,
                 n_steps=2048)
-    model.learn(total_timesteps=int(1.2e6))  # , callback=TensorboardCallback())
+    model.learn(total_timesteps=int(1.2e6),  callback=TensorboardCallback())  # , callback=TensorboardCallback())
 
     # save the model
     model_path = os.path.join(path, "PPO.zip")
