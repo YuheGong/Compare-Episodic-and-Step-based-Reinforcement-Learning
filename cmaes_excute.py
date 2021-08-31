8c5a9822f91ed7da22d35187acc383825fab7179
YRE <gongyuhe1023@gmail.com>
2021-09-01 00:04:15 +0200

    some changes

--- cmaes_excute.py
+++ cmaes_excute.py
@@ -2,7 +2,7 @@ import os
 
 run = 20
 for i in range(6):
-    str = f'python cw2cmaes.py cw2cames/dmp_{i}.yml'
+    str = f'python cmaes_cw2.py cw2cames/dmp_{i}.yml'
     os.system(str)
-    str = f'python cw2cmaes.py cw2cames/promp_{i}.yml'
+    str = f'python cmaes_cw2.py cw2cames/promp_{i}.yml'
     os.system(str)
