c8c0068abae9dc45096d13a8f52e102ff8cc9242
YRE <gongyuhe1023@gmail.com>
2022-05-03 15:45:17 +0200

    som ehcanges

--- cw2run.py
+++ cw2run.py
@@ -4,6 +4,6 @@ for i in range(5):
     #str = f'python train.py --algo sac --env_id ALRReacherBalanceIP-v3 --seed {i}'
     str = f'python train.py --algo sac --env_id dmcCheetahDense-v0 --seed {i}'
     #str = f'python train.py --algo promp --env_id FetchReacherProMP-v1 --seed {i}'
-    #os.system(str)
+    os.system(str)
     #str = f'python cw2cmaes/cmaes_cw2.py cw2cmaes/promp_dense_{i}.yml -o'
     #os.system(str)
