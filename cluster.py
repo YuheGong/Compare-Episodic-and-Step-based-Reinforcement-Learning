b296eb338629f4aa01bbe3ffc9dc702eb316eb09
YuheGong <gongyuhe1023@gmail.com>
2022-07-28 11:16:06 +0200

    sme changes

--- cluster.py
+++ cluster.py
@@ -1,17 +1,18 @@
 import os
 
 for i in range(5):
-    str = f'python train.py --algo promp --env Meta-promp-dense-soccer-v2 --seed {i}'
+    str = f'python train.py --algo sac --e HopperXYJumpStep-v0 --seed {i}'
     os.system(str)
 
-    str = f'python train.py --algo promp --env Meta-promp-soccer-v2 --seed {i}'
-    os.system(str)
+    #str = f'python train.py --algo promp --env Meta-promp-soccer-v2 --seed {i}'
+    #os.system(str)
 
 
-
+'''
 for i in range(5):
     str = f'python train.py --algo promp --env Meta-promp-dense-coffee-push-v2 --seed {i}'
     os.system(str)
 
     str = f'python train.py --algo promp --env Meta-promp-coffee-push-v2 --seed {i}'
     os.system(str)
+'''
